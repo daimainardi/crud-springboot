c552f1c3ec15caf3e5ed2a73be48732db2e3210e
Daiane Mainardi <daimainardi@hotmail.com>
2023-07-20 11:41:37 -0300

    Category

--- src/main/java/com/daimainardi/crudspringboot/config/TestConfig.java
+++ src/main/java/com/daimainardi/crudspringboot/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.daimainardi.crudspringboot.config;
 
+import com.daimainardi.crudspringboot.entity.Category;
 import com.daimainardi.crudspringboot.entity.Order;
 import com.daimainardi.crudspringboot.entity.User;
 import com.daimainardi.crudspringboot.entity.enums.OrderStatus;
+import com.daimainardi.crudspringboot.repository.CategoryRepository;
 import com.daimainardi.crudspringboot.repository.OrderRepository;
 import com.daimainardi.crudspringboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,8 @@ public class TestConfig implements CommandLineRunner {
     private UserRepository userRepository;
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -30,8 +34,14 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(u1, u2));
-        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
 
     }
 }
--- src/main/java/com/daimainardi/crudspringboot/resource/CategoryResource.java
+++ src/main/java/com/daimainardi/crudspringboot/resource/CategoryResource.java
@@ -1,7 +1,8 @@
 package com.daimainardi.crudspringboot.resource;
 
+import com.daimainardi.crudspringboot.entity.Category;
 import com.daimainardi.crudspringboot.entity.User;
-import com.daimainardi.crudspringboot.service.UserService;
+import com.daimainardi.crudspringboot.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,20 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/categories")
+public class CategoryResource {
 
     @Autowired
-    private UserService service;
+    private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        List<User> list = service.findAll();
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-        User obj = service.findById(id);
+    public ResponseEntity<Category> findById(@PathVariable Long id){
+        Category obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
