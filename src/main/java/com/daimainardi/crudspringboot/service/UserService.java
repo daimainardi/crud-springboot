a1100513fbbb909b6bb8a74d7cb097090de24a85
Daiane Mainardi <daimainardi@hotmail.com>
2023-07-24 18:36:47 -0300

    Exception handling - findById

--- src/main/java/com/daimainardi/crudspringboot/service/UserService.java
+++ src/main/java/com/daimainardi/crudspringboot/service/UserService.java
@@ -2,10 +2,12 @@ package com.daimainardi.crudspringboot.service;
 
 import com.daimainardi.crudspringboot.entity.User;
 import com.daimainardi.crudspringboot.repository.UserRepository;
+import com.daimainardi.crudspringboot.service.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,9 +18,9 @@ public class UserService {
         return repository.findAll();
     }
     public User findById(Long id){
-        return repository.findById(id).get();
-        //Optional<Order> obj = repository.findById(id);
-        //        return obj.get();
+        //return repository.findById(id).get();
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public User insert(User obj){
         return repository.save(obj);
